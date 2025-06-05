5611dce5b8cf02bceceba3e2382f802774cf4d5e
Valentina <dysfnalv@gmail.com>
2025-06-04 21:00:38 -0500

    Added gitignore, added scripts for dev and prod envmts

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config({path: './.env'});
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+    console.log(`Environment running: ${process.env.NODE_ENV}`);
+});
