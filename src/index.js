feae30991725c8cbd48cfff71c14996a3048932e
Valentina <dysfnalv@gmail.com>
2025-06-04 21:40:20 -0500

    Created search controller and route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const searchRoutes = require('./routes/searchRoutes');
 const dotenv = require('dotenv');
 dotenv.config({path: './.env'});
 
 const app = express();
+
+//register routes
+app.use('/api/v1/search', searchRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
